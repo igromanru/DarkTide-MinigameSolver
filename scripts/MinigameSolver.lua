22bec3671571ad172326e70b57a405649e6654c5
igromanru <igromanru@yahoo.de>
2024-11-16 00:20:59 +0100

    feat: Trying to improve

--- scripts/MinigameSolver.lua
+++ scripts/MinigameSolver.lua
@@ -5,21 +5,25 @@ local SettingNames = mod:io_dofile("MinigameSolver/scripts/setting_names")
 local decode_on_taget = false
 
 local cooldown = 0.0
-function mod.update(dt)
+function mod.update(delta_time)
     if cooldown > 0 then
-        cooldown = cooldown - dt
+        cooldown = cooldown - delta_time
     end
 end
 
 mod:hook_safe(CLASS.MinigameDecodeSymbolsView, "_draw_cursor", function(self, widgets_by_name, decode_start_time, on_target, gameplay_time)
 	mod:debug("--- [MinigameDecodeSymbolsView._draw_cursor] ---")
-    decode_on_taget = on_target
+    if cooldown <= 0 and on_target then
+        decode_on_taget = true
+    else
+        decode_on_taget = false
+    end
     mod:debug("--------------------")
 end)
 
 mod:hook("InputService", "_get", function(func, self, action_name)
-    if cooldown <= 0 and decode_on_taget and action_name == "action_one_hold" then
-        cooldown = 0.05
+    if decode_on_taget and action_name == "action_one_hold" then
+        cooldown = 0.1
         decode_on_taget = false
         return true 
     end
